dc16c18a201808835f103a2c203ed9d4bc63cfc3
Jefferson Rios <jrios@sumset.com>
2020-11-28 10:38:39 -0500

    Estructura inicial

---
+++ src/main/java/com/ceiba/biblioteca/BibliotecaCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.ceiba.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaCeibaApplication.class, args);
+	}
+
+}
