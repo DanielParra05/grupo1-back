6e2dc83b8ba69017c1a77180a41211f022b968ba
nancymarcela20 <nancymarcelaag@ufps.edu.co>
2020-11-28 15:00:51 -0500

    Se adicionó package de exception

---
+++ src/main/java/com/ceiba/library/exception/loadException.java
@@ -0,0 +1,10 @@
+package com.ceiba.library.exception;
+
+public class loadException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public loadException(String message) {
+		super(message);
+	}
+}
